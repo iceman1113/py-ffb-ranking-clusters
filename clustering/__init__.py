19d85ce788a2a5efaefca51b4e6f234259ac054b
iceman1113 <30938095+iceman1113@users.noreply.github.com>
2022-09-03 13:55:37 -0400

    Update __init__.py

---
+++ clustering/__init__.py
@@ -0,0 +1,11 @@
+"""
+A collection of modules to cluster data using k-means
+"""
+# Expose cluster.Cluster class.
+from .cluster import Cluster
+
+# Expose kmeans.KMeans class.
+from .kmeans import KMeans
+
+# Expose ClusterApproximator class.
+from .cluster_approximator import ClusterApproximator
